78caa57bde11ff666298adf681d7d23be9ec545f
LenkaKocmanova <lenka.kocmanova@gmail.com>
2021-11-15 10:45:33 +0100

    ukol 8

---
+++ ukol9/lekce9.py
@@ -0,0 +1,41 @@
+import pandas
+import requests
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+from sklearn.metrics import (
+    accuracy_score,
+    confusion_matrix,
+    f1_score,
+    precision_score,
+    ConfusionMatrixDisplay,
+)
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import StandardScaler
+
+
+r = requests.get("https://raw.githubusercontent.com/lutydlitatova/czechitas-datasets/main/datasets/water-potability.csv")
+open("water-potability.csv", 'wb').write(r.content)
+
+data = pandas.read_csv("water-potability.csv")
+data.shape
+data = data.dropna()
+data.shape
+
+X = data.drop(columns=["Potability"])
+y = data["Potability"]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.3, random_state=42
+)
+
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+clf = KNeighborsClassifier(3)
+clf.fit(X_train, y_train)
+
+y_pred = clf.predict(X_test)
+print(f1_score(y_test, y_pred))
